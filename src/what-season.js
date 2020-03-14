ac6e434ef94bf0f8a2715cdfc3b3d7d1664890bf
PavelZakharov <pavelzakharov1987@gmail.com>
2020-03-15 01:36:09 +0300

    feat: add hanoi and what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,23 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+  if (!date) { 
+    return 'Unable to determine the time of year!'; 
+  }
+  if (Object.prototype.toString.call(date) != '[object Date]') {
+    throw Error;
+  }
+
+  let month = date.getMonth();
+
+  if ((month >= 0 && month <= 1) || month == 11) { 
+    return 'winter';
+  }
+  if (month >= 2 && month <= 4) {
+    return 'spring';
+  }
+  if (month >= 5 && month <= 7) {
+    return 'summer';
+  }
+  if (month >= 8 && month <= 10) {
+    return 'autumn';
+  }
+}; 
