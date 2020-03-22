4b9eff2eb2bd85fe8929071804af653cebedae3d
PavelZakharov <pavelzakharov1987@gmail.com>
2020-03-22 14:45:38 +0300

    feat: add extended-repeater and recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,14 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+        let depth = 1;
+        arr.forEach(element => {
+            if (Array.isArray(element)) {
+                let nextDepth = this.calculateDepth(element);
+                if (depth < nextDepth + 1) {
+                    depth = nextDepth + 1;
+                }
+            }
+        });
+        return depth;
     }
 };
